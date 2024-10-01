3104ed97cbf258aa6f96a9dcfc30bd45b7fa72b5
Timas Stankevicius <timothystankus444@gmail.com>
2024-10-01 15:27:34 +0300

    npm-init+cypress-install

---
+++ cypress/e2e/signup-page/register.cy.js
@@ -0,0 +1,11 @@
+const pageUrl = "https://www.automationexercise.com";
+
+describe("user register", () => {
+  beforeEach(() => {
+    cy.visit(pageUrl);
+  });
+
+  it('successfully registers a new user', () => {
+    
+  });
+});
